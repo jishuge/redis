a617f030235e157d57816a957c2cf75ddf253a17
季书歌 <18108447675@163.com>
2022-07-11 18:21:14 +0800

    第三个版本

--- redis.c
+++ redis.c
@@ -1663,7 +1663,7 @@ static int rdbSaveLzfStringObject(FILE *fp, robj *obj) {
     /* We require at least four bytes compression for this to be worth it */
     outlen = sdslen(obj->ptr)-4;
     if (outlen <= 0) return 0;
-    if ((out = zmalloc(outlen)) == NULL) return 0;
+    if ((out = zmalloc(outlen+1)) == NULL) return 0;
     comprlen = lzf_compress(obj->ptr, sdslen(obj->ptr), out, outlen);
     if (comprlen == 0) {
         zfree(out);
